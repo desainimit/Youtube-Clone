342d6c8c47c3303cb7ee5f58931c1ad72cbb2e4c
= <=>
2024-08-05 18:22:29 +0530

    middleware config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import dotenv from "dotenv";
-import express from "express";
 
 import connectDB from "./db/dbConnect.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server running on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((error) => console.error("MongoDB connection Failed:", error));
