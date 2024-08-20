d0e102faaead5d517260aad68c16c627b73c2f88
= <=>
2024-08-20 13:00:56 +0530

    chore: Add express-async-handler package for handling asynchronous requests

---
+++ src/utils/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+  constructor(status, message = "Something went wrong", errors = []) {
+    super();
+    this.status = status;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+  }
+  static badRequest(message) {
+    return new ApiError(400, message);
+  }
+  static internal(message) {
+    return new ApiError(500, message);
+  }
+  static notFound(message) {
+    return new ApiError(404, message);
+  }
+}
+
+export { ApiError };
